9077185d9f1e0eb0b2ed8bfc46850aab1bc09f26
Ryperhun <akormanyzsolt@gmail.com>
2017-05-24 22:14:31 +0200

    Added boxTest description

--- 01_Shadows/FrustumG.h
+++ 01_Shadows/FrustumG.h
@@ -46,8 +46,48 @@ public:
 	bool pointInFrustum(vec3 &p);
 
 	////int sphereInFrustum(vec3 &p, float raio);
-	//int boxInFrustum(AABox &b)
-	//{
-	//	
+	///SAT is faster
+	//http://www.dyn4j.org/2010/01/sat/
+	//int FrustumG::boxInFrustum(Box &b) {
+
+	//	int result = INSIDE, out, in;
+
+	//	// for each plane do ...
+	//	for (int i = 0; i < 6; i++) {
+
+	//		// reset counters for corners in and out
+	//		out = 0; in = 0;
+	//		// for each corner of the box do ...
+	//		// get out of the cycle as soon as a box as corners
+	//		// both inside and out of the frustum
+	//		for (int k = 0; k < 8 && (in == 0 || out == 0); k++) {
+
+	//			// is the corner outside or inside
+	//			if (pl[i].distance(b.getVertex(k)) < 0)
+	//				out++;
+	//			else
+	//				in++;
+	//		}
+	//		//if all corners are out
+	//		if (!in)
+	//			return (OUTSIDE);
+	//		// if some corners are out and others are in
+	//		else if (out)
+	//			result = INTERSECT;
+	//	}
+	//	return(result);
 	//}
+
+	//Sat code
+	//for (int i = 0; i < 6; ++i) {
+	//	const Plane& plane = frustum.planes[i];
+
+	//	float d = glm::dot(center, plane.normal);
+	//	float r = glm::dot(extent, glm::abs(plane.normal));
+
+	//	if (d + r < -plane.dist) {
+	//		return false;
+	//	}
+	//}
+	//return true;
 };
