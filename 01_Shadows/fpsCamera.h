fa939b58d1ff9d4af8d09d36b6eb8318d7bc05fe
Ryperhun <akormanyzsolt@gmail.com>
2017-06-14 14:14:46 +0200

    FPS Camera fully working

--- 01_Shadows/fpsCamera.h
+++ 01_Shadows/fpsCamera.h
@@ -8,39 +8,45 @@
 
 struct FPSCamera
 {
-	//For View
-	float pitch = 0, yaw = 0, roll = 0;
-	//glm::quat cameraQuat;
-	glm::vec3	eyePos;
+	//For lookAt
+	glm::vec3 forward;
+	glm::vec3 eyePos;
 	//For Projection
 	float aspectRatio;
 	float zNear, zFar;
 	float fovDegree = 90.0f; //In degree
+	//For Movement
+	glm::vec3 moveDir;
+	float cameraSpeed = 5.0f;
+	const glm::vec3 globalUp;
+
+	/// The view matrix of the camera
+	glm::mat4	viewMatrix;
+	glm::mat4	projMatrix;
+	glm::mat4	projViewMatrix;
+	glm::mat4   rayDirMatrix; //viewDir = rayDirMtx * pos_ndc;
+
+	//FrustumG frustum; ///TODO Megold frustum calculationt
 public:
 	FPSCamera(float zNear, float zFar, float width, float height)
-		:zNear(zNear), zFar(zFar)
+		:zNear(zNear), zFar(zFar), globalUp(0, 1, 0)
+		,forward(0,0,1)
 	{
 		Resize(width, height);
+		eyePos = glm::vec3(5, 22, 24);
+
 		UpdateViewMatrix();
 	}
 	//FPSCamera(glm::vec3 eye, glm::vec3 at, glm::vec3 up);
 
-	void UpdateViewMatrix()
+	void UpdateViewMatrix(float yaw = 0.0f, float pitch = 0.0f)
 	{
-		//temporary frame quaternion from pitch,yaw,roll 
-		//here roll is not used
-		glm::quat qPitch = glm::angleAxis(pitch, glm::vec3(1, 0, 0));
-		glm::quat qYaw = glm::angleAxis(yaw, glm::vec3(0, 1, 0));
-		//glm::quat qRoll = glm::angleAxis(roll, glm::vec3(0, 0, 1));
+		glm::vec3 right = GetRightVec(forward);
+		glm::vec3 up = GetUpVec(forward, right);
 
-		glm::quat orientation = qPitch * qYaw;
-		orientation = glm::normalize(orientation);
-		glm::mat4 rotate = glm::mat4_cast(orientation);
-
-		glm::mat4 translate = glm::mat4(1.0f);
-		translate = glm::translate(translate, -eyePos);
-
-		viewMatrix = rotate * translate; //Same as glm::lookAt(), just with quaternions
+		forward = forward + yaw * right + up * pitch;
+		forward = glm::normalize(forward);
+		viewMatrix = glm::lookAt(eyePos, eyePos + forward, globalUp);
 	}
 	void UpdateProjMatrix()
 	{
@@ -54,14 +60,30 @@ public:
 
 	void Update(float deltaTime)
 	{
-		///TODO add movement
+		glm::vec3 right = GetRightVec(forward);
+		glm::vec3 up    = GetUpVec (forward, right);
+
+		float velocity = deltaTime * cameraSpeed;
+		eyePos += moveDir.x * velocity * right;
+		eyePos += moveDir.y * velocity * globalUp;
+		eyePos += moveDir.z * velocity * forward;
+
 		UpdateViewMatrix();
 		UpdateProjMatrix();
 		projViewMatrix = projMatrix * viewMatrix;
 
 		rayDirMatrix = glm::inverse(projViewMatrix *  glm::translate(eyePos));
 	}
-	
+
+	//In cameraCoord
+	glm::vec3 GetRightVec(glm::vec3 forward) 
+	{
+		return glm::normalize(glm::cross(forward, globalUp));
+	}
+	glm::vec3 GetUpVec (glm::vec3 forward, glm::vec3 right)
+	{
+		return glm::normalize(glm::cross(right, forward));
+	}
 
 	glm::mat4 GetViewMatrix(){return viewMatrix;}
 	glm::mat4 GetProj()	{return projMatrix;	}
@@ -71,9 +93,63 @@ public:
 
 
 	void KeyboardDown(SDL_KeyboardEvent& key)
-	{}
+	{
+		switch (key.keysym.sym)
+		{
+		case SDLK_LSHIFT:
+		case SDLK_RSHIFT:
+			//if (!m_slow)
+			//{
+			//	m_slow = true;
+			//	m_speed /= 4.0f;
+			//}
+			break;
+		case SDLK_w:
+			moveDir.z = 1.0f;
+			break;
+		case SDLK_s:
+			moveDir.z = -1.0f;
+			break;
+		case SDLK_a:
+			moveDir.x = -1.0f;
+			break;
+		case SDLK_d:
+			moveDir.x = 1.0f;
+			break;
+		case SDLK_e:
+			moveDir.y = 1.0f;
+			break;
+		case SDLK_q:
+			moveDir.y = -1.0f;
+			break;
+		}
+	}
 	void KeyboardUp(SDL_KeyboardEvent& key)
-	{}
+	{
+		switch (key.keysym.sym)
+		{
+		case SDLK_LSHIFT:
+		case SDLK_RSHIFT:
+			//if (!m_slow)
+			//{
+			//	m_slow = true;
+			//	m_speed /= 4.0f;
+			//}
+			break;
+		case SDLK_w:
+		case SDLK_s:
+			moveDir.z = 0.0f;
+			break;
+		case SDLK_a:
+		case SDLK_d:
+			moveDir.x = 0.0f;
+			break;
+		case SDLK_e:
+		case SDLK_q:
+			moveDir.y = 0.0f;
+			break;
+		}
+	}
 	void MouseMove(SDL_MouseMotionEvent& mouse)
 	{
 		if (mouse.state & SDL_BUTTON_LMASK)
@@ -81,22 +157,179 @@ public:
 			//TODO Sensitivity
 			glm::vec2 mouseDelta(mouse.xrel / 100.0f, mouse.yrel / 100.0f);
 
-			yaw   += mouseDelta.x;
-			pitch += mouseDelta.y;
-			UpdateViewMatrix();
+			UpdateViewMatrix(mouseDelta.x, -mouseDelta.y);
 		}
 	}
-
-public:
-
-	///  The traversal speed of the camera
-	float		m_speed;
-	/// The view matrix of the camera
-	glm::mat4	viewMatrix;
-	glm::mat4	projMatrix;
-	glm::mat4	projViewMatrix;
-	glm::mat4   rayDirMatrix; //viewDir = rayDirMtx * pos_ndc;
-
-	//FrustumG frustum; ///TODO Megold frustum calculationt
 };
 
+//struct FPSCamera
+//{
+//	//For View
+//	float pitch = 0, yaw = 0, roll = 0;
+//	//glm::quat cameraQuat;
+//	glm::vec3	eyePos;
+//	//For Projection
+//	float aspectRatio;
+//	float zNear, zFar;
+//	float fovDegree = 90.0f; //In degree
+//							 //For Movement
+//	glm::vec3 moveDir;
+//	float cameraSpeed = 5.0f;
+//	glm::vec3 axisX, axisZ, axisY; //CameraDirection == axisZ
+//	const glm::vec3 globalUp;
+//public:
+//	FPSCamera(float zNear, float zFar, float width, float height)
+//		:zNear(zNear), zFar(zFar), globalUp(0, 1, 0)
+//	{
+//		Resize(width, height);
+//		UpdateViewMatrix();
+//	}
+//	//FPSCamera(glm::vec3 eye, glm::vec3 at, glm::vec3 up);
+//
+//	void UpdateViewMatrix()
+//	{
+//		//temporary frame quaternion from pitch,yaw,roll 
+//		//here roll is not used
+//		glm::quat qPitch = glm::angleAxis(pitch, glm::vec3(1, 0, 0));
+//		glm::quat qYaw = glm::angleAxis(yaw, glm::vec3(0, 1, 0));
+//		//glm::quat qRoll = glm::angleAxis(roll, glm::vec3(0, 0, 1));
+//
+//		glm::quat orientation = qPitch * qYaw;
+//		orientation = glm::normalize(orientation);
+//		glm::mat4 rotate = glm::mat4_cast(orientation);
+//
+//		glm::mat4 translate = glm::mat4(1.0f);
+//		translate = glm::translate(translate, -eyePos);
+//
+//		viewMatrix = rotate * translate; //Same as glm::lookAt(), just with quaternions
+//	}
+//	void UpdateProjMatrix()
+//	{
+//		projMatrix = glm::perspective(glm::radians(fovDegree), aspectRatio, zNear, zFar);
+//	}
+//	void Resize(int w, int h)
+//	{
+//		aspectRatio = w / (float)h;
+//		UpdateProjMatrix();
+//	}
+//
+//	void Update(float deltaTime)
+//	{
+//		//if (pitch > 90)
+//		//	pitch = 90;
+//		//if (pitch < -90)
+//		//	pitch = -90;
+//		CalculateAxisVectors();
+//		float velocity = deltaTime * cameraSpeed;
+//		eyePos += moveDir.x * velocity * axisZ;
+//		eyePos += moveDir.y * velocity * globalUp;
+//		eyePos += moveDir.z * velocity * axisX;
+//		///TODO add movement
+//		UpdateViewMatrix();
+//		UpdateProjMatrix();
+//		projViewMatrix = projMatrix * viewMatrix;
+//
+//		rayDirMatrix = glm::inverse(projViewMatrix *  glm::translate(eyePos));
+//	}
+//
+//	void CalculateAxisVectors()
+//	{
+//		float yawRad = glm::radians(yaw);
+//		float pitchRad = glm::radians(pitch);
+//		axisZ = glm::vec3(cos(yaw)*cos(pitch), sin(pitch), sin(yaw)*cos(pitch));
+//		//axisZ = glm::vec3(sin(yawRad)*cos(pitchRad), sin(pitchRad), cos(yawRad)*cos(pitchRad));
+//		//axisZ = glm::vec3(cos(yawRad)*cos(pitchRad), sin(pitchRad), cos(yawRad)*sin(pitchRad));
+//		axisX = glm::normalize(glm::cross(-axisZ, globalUp));
+//		axisY = glm::cross(axisX, axisZ);
+//	}
+//
+//	glm::mat4 GetViewMatrix() { return viewMatrix; }
+//	glm::mat4 GetProj() { return projMatrix; }
+//	glm::mat4 GetProjView() { return projViewMatrix; }
+//	glm::mat4 GetRayDirMtx() { return rayDirMatrix; }
+//	glm::vec3 GetEye() { return eyePos; }
+//
+//
+//	void KeyboardDown(SDL_KeyboardEvent& key)
+//	{
+//		switch (key.keysym.sym)
+//		{
+//		case SDLK_LSHIFT:
+//		case SDLK_RSHIFT:
+//			//if (!m_slow)
+//			//{
+//			//	m_slow = true;
+//			//	m_speed /= 4.0f;
+//			//}
+//			break;
+//		case SDLK_w:
+//			moveDir.z = 1.0f;
+//			break;
+//		case SDLK_s:
+//			moveDir.z = -1.0f;
+//			break;
+//		case SDLK_a:
+//			moveDir.x = -1.0f;
+//			break;
+//		case SDLK_d:
+//			moveDir.x = 1.0f;
+//			break;
+//		case SDLK_e:
+//			moveDir.y = 1.0f;
+//			break;
+//		case SDLK_q:
+//			moveDir.y = -1.0f;
+//			break;
+//		}
+//	}
+//	void KeyboardUp(SDL_KeyboardEvent& key)
+//	{
+//		switch (key.keysym.sym)
+//		{
+//		case SDLK_LSHIFT:
+//		case SDLK_RSHIFT:
+//			//if (!m_slow)
+//			//{
+//			//	m_slow = true;
+//			//	m_speed /= 4.0f;
+//			//}
+//			break;
+//		case SDLK_w:
+//		case SDLK_s:
+//			moveDir.z = 0.0f;
+//			break;
+//		case SDLK_a:
+//		case SDLK_d:
+//			moveDir.x = 0.0f;
+//			break;
+//		case SDLK_e:
+//		case SDLK_q:
+//			moveDir.y = 0.0f;
+//			break;
+//		}
+//	}
+//	void MouseMove(SDL_MouseMotionEvent& mouse)
+//	{
+//		if (mouse.state & SDL_BUTTON_LMASK)
+//		{
+//			//TODO Sensitivity
+//			glm::vec2 mouseDelta(mouse.xrel / 100.0f, mouse.yrel / 100.0f);
+//
+//			yaw += mouseDelta.x;
+//			pitch += mouseDelta.y;
+//			UpdateViewMatrix();
+//		}
+//	}
+//
+//public:
+//
+//	///  The traversal speed of the camera
+//	float		m_speed;
+//	/// The view matrix of the camera
+//	glm::mat4	viewMatrix;
+//	glm::mat4	projMatrix;
+//	glm::mat4	projViewMatrix;
+//	glm::mat4   rayDirMatrix; //viewDir = rayDirMtx * pos_ndc;
+//
+//							  //FrustumG frustum; ///TODO Megold frustum calculationt
+//};
