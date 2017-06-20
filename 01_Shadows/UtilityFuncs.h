755fee1dd052d95bbcb80893a3737c59d2dc2620
Ryperhun <akormanyzsolt@gmail.com>
2017-06-20 21:27:46 +0200

    Added integer random generation for util functions

--- 01_Shadows/UtilityFuncs.h
+++ 01_Shadows/UtilityFuncs.h
@@ -3,6 +3,21 @@
 #include <cstdio>
 
 struct Util {
+	//Returns [lowerBound, upperBound]
+	inline static int randomPointI(int lowerBound, int upperBound)
+	{
+		int interval = upperBound - lowerBound; //5 - 1 = 4
+		int num = rand() % (interval + 1) + lowerBound; //4 % (4 + 1) = 4 + 1 = 5
+		return num;
+	}
+	inline static glm::ivec3 randomVec3I(int lowerBound, int upperBound)
+	{
+		glm::ivec3 vector (Util::randomPointI(lowerBound, upperBound),
+							Util::randomPointI(lowerBound, upperBound),
+							Util::randomPointI(lowerBound, upperBound));
+		return vector;
+	}
+	//[0, 1]
 	static float randomPoint();
 	//returns random vec [-1,1]
 	static glm::vec3 randomVec();
