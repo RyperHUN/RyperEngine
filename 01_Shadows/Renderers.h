8f9aa54810107f53012e84a9b6520f176a1bad6c
Ryperhun <akormanyzsolt@gmail.com>
2017-07-13 14:08:03 +0200

    Refactored LightRenderer to OGLWRAP

--- 01_Shadows/Renderers.h
+++ 01_Shadows/Renderers.h
@@ -17,82 +17,74 @@ public:
 	{}
 	void Render(glm::mat4 VP, CameraPtr camera)
 	{
-		FrustumG* frustum = camera->GetFrustum ();
 		shader->On();
 		shader->SetUniform ("PVM", VP);
 		shader->SetUniform ("isSelected", true);
 		{
-			//gl::VertexArray VAO;
-			//gl::Bind(VAO);
-			//gl::ArrayBuffer VBO;
-			//gl::Bind(VBO);
-			//gl::VertexAttrib attrib(0);
-			//attrib.enable();
+			gl::VertexArray VAO;
+			gl::Bind(VAO);
+			{
+				gl::ArrayBuffer VBO;
+				gl::Bind(VBO);
+				{
+					std::vector<glm::vec3> triangles;
+					FillVectorWithFrustumData (triangles, camera->GetFrustum());
 
-			gVertexBuffer buffer;
+					gl::VertexAttrib attrib(0);
+					attrib.pointer(3, gl::kFloat);
+					attrib.enable();
+					VBO.data (triangles, gl::kDynamicDraw);
+
 			
-			std::vector<glm::vec3> triangles;
-			triangles.push_back(frustum->nbr);	//Right side
-			triangles.push_back(frustum->fbr);
-			triangles.push_back(frustum->ftr);
-
-			triangles.push_back(frustum->ntr);
-			/////////////////////////////////////////
-			//Left side
-			triangles.push_back(frustum->nbl);
-			triangles.push_back(frustum->ntl);
-			triangles.push_back(frustum->ftl);
-			triangles.push_back(frustum->fbl);
-
-			//Front
-			triangles.push_back(frustum->ntl);
-			triangles.push_back(frustum->nbl);
-			triangles.push_back(frustum->nbr);
-			triangles.push_back(frustum->ntr);
-
-			//Back
-			triangles.push_back(frustum->ftl);
-			triangles.push_back(frustum->fbl);
-			triangles.push_back(frustum->fbr);
-			triangles.push_back(frustum->ftr);
-
-			//Top
-			triangles.push_back(frustum->ntl);
-			triangles.push_back(frustum->ntr);
-			triangles.push_back(frustum->ftr);
-			triangles.push_back(frustum->ftl);
-
-			//Bottom
-			triangles.push_back(frustum->nbl);
-			triangles.push_back(frustum->fbl);
-			triangles.push_back(frustum->fbr);
-			triangles.push_back(frustum->nbr);
-
-			buffer.AddAttribute(0, 3);
-			for(int i = 0 ; i < triangles.size(); i++)
-				buffer.AddData(0, triangles[i]);
-
-			buffer.InitBuffers();
-
-			buffer.On();
-			glDisable(GL_CULL_FACE);
-			glEnable(GL_BLEND);
-				buffer.Draw (GL_QUADS);
-			glDisable(GL_BLEND);
-			glEnable(GL_CULL_FACE);
-			buffer.Off();
-
-			//VBO.setup (3, gl::kFloat, 0, &triangles[0]);
-			//gl::VertexAttrib(0).pointer(
-			//	3, gl::kFloat, false, 0, 0).enable();
-
-			//VBO.data(triangles, gl::kDynamicDraw);
-
-			//gl::DrawArrays (gl::kTriangles, 0, triangles.size() * 3);
-			//gl::Unbind(VBO);
-			//gl::Unbind(VAO);
+					glDisable(GL_CULL_FACE);
+					glEnable(GL_BLEND);
+						gl::DrawArrays(gl::kTriangleFan, 0, triangles.size() * 3);
+					glDisable(GL_BLEND);
+					glEnable(GL_CULL_FACE);
+				}
+				gl::Unbind(VBO);
+			}
+			gl::Unbind(VAO);
 		}
 		shader->Off();
+	}
+	static void FillVectorWithFrustumData (std::vector<glm::vec3> &triangles, FrustumG* frustum)
+	{
+		triangles.push_back(frustum->nbr);	//Right side
+		triangles.push_back(frustum->fbr);
+		triangles.push_back(frustum->ftr);
+
+		triangles.push_back(frustum->ntr);
+		/////////////////////////////////////////
+		//Left side
+		triangles.push_back(frustum->nbl);
+		triangles.push_back(frustum->ntl);
+		triangles.push_back(frustum->ftl);
+		triangles.push_back(frustum->fbl);
+
+		//Front
+		triangles.push_back(frustum->ntl);
+		triangles.push_back(frustum->nbl);
+		triangles.push_back(frustum->nbr);
+		triangles.push_back(frustum->ntr);
+
+		//Back
+		triangles.push_back(frustum->ftl);
+		triangles.push_back(frustum->fbl);
+		triangles.push_back(frustum->fbr);
+		triangles.push_back(frustum->ftr);
+
+		//Top
+		triangles.push_back(frustum->ntl);
+		triangles.push_back(frustum->ntr);
+		triangles.push_back(frustum->ftr);
+		triangles.push_back(frustum->ftl);
+
+		//Bottom
+		triangles.push_back(frustum->nbl);
+		triangles.push_back(frustum->fbl);
+		triangles.push_back(frustum->fbr);
+		triangles.push_back(frustum->nbr);
 	}
 };
 
