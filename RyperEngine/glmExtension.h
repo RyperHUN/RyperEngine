76ae83cf69c97d79c8a3e883808c1446b6dbb2a1
Ryperhun <akormanyzsolt@gmail.com>
2017-08-21 11:11:34 +0200

    Fixed glmExtensions constructors

--- RyperEngine/glmExtension.h
+++ RyperEngine/glmExtension.h
@@ -11,11 +11,20 @@ namespace glm
 		ivec2XZ(int x, int z)
 			:x(x),z(z)
 		{}
-		ivec2XZ(vec2 const& vec)
+		explicit ivec2XZ(ivec2 const& vec)
 			:x(vec.x), z(vec.y)
 		{}
-		ivec2XZ(vec3 const& vec)
+		ivec2XZ(ivec3 const& vec)
 			:x(vec.x), z(vec.z)
 		{}
+		bool operator<(glm::ivec2XZ const& rhs) const
+		{
+			if(x < rhs.x)
+				return true;
+			if (z < rhs.z)
+				return true;
+
+			return false;
+		}
 	};
 }
