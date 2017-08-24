2c5e023a832779e2d77cb6283f7093d52d476c4a
Ryperhun <akormanyzsolt@gmail.com>
2017-08-24 17:37:30 +0200

    Rendering preparation in particle render refactored

--- RyperEngine/Particles.h
+++ RyperEngine/Particles.h
@@ -7,88 +7,110 @@ namespace Engine
 namespace Particle
 {
 	
-	struct Particle 
+struct Particle 
+{
+	glm::vec3 position;
+	glm::vec3 velocity;
+	float gravityEffect;
+	float lifeLength;
+	float rotationZ;
+	float scale;
+	float elapsedTime = 0;
+	Particle (glm::vec3 pos, glm::vec3 velocity, float gravityEffect, float lifeLength, float rotationZ = 0, float scale = 1)
+		:position(pos), velocity(velocity), gravityEffect (gravityEffect), lifeLength(lifeLength), rotationZ(rotationZ), scale(scale)
+	{}
+	//Returns true if alive
+	bool Update (float dt)
 	{
-		glm::vec3 position;
-		glm::vec3 velocity;
-		float gravityEffect;
-		float lifeLength;
-		float rotationZ;
-		float scale;
-		float elapsedTime = 0;
-		Particle (glm::vec3 pos, glm::vec3 velocity, float gravityEffect, float lifeLength, float rotationZ = 0, float scale = 1)
-			:position(pos), velocity(velocity), gravityEffect (gravityEffect), lifeLength(lifeLength), rotationZ(rotationZ), scale(scale)
-		{}
-		//Returns true if alive
-		bool Update (float dt)
-		{
-			static const float GRAVITY = -10.0f;
-			velocity.y = velocity.y + GRAVITY * gravityEffect * dt;
-			glm::vec3 movementDt = velocity * dt;
-			position = position + movementDt;
-			elapsedTime += dt;
+		static const float GRAVITY = -10.0f;
+		velocity.y = velocity.y + GRAVITY * gravityEffect * dt;
+		glm::vec3 movementDt = velocity * dt;
+		position = position + movementDt;
+		elapsedTime += dt;
 
-			return IsAlive();
-		}
-		bool IsAlive ()
-		{
-			return elapsedTime < lifeLength;
-		}
-	};
-
-	struct ParticleRenderer
+		return IsAlive();
+	}
+	bool IsAlive ()
 	{
-		QuadTexturer &quadTexturer;
-		GLuint texture;
-		std::vector<Particle> particles;
-		ParticleRenderer (QuadTexturer & quadTexturer)
-			:quadTexturer(quadTexturer)
+		return elapsedTime < lifeLength;
+	}
+};
+
+enum RenderType
+{
+	ADDITIVE, //Shiny feeling
+	ALPHA_BLENDED // Ordered
+};
+
+class ParticleRenderer
+{
+	
+	QuadTexturer &quadTexturer;
+	GLuint texture;
+	std::vector<Particle> particles;
+	RenderType type;
+public:
+	ParticleRenderer (QuadTexturer & quadTexturer, RenderType type)
+		:quadTexturer(quadTexturer), type(type)
+	{
+	}
+	void Init (GLuint texture)
+	{
+		this->texture = texture;
+		const int particleNum = 40;
+		particles.reserve (particleNum);
+		for (int i = 0 ; i < particleNum; i++)
 		{
+			particles.push_back(GenParticle ());
 		}
-		void Init (GLuint texture)
+	}
+	Particle GenParticle ()
+	{
+		glm::vec3 initPos = glm::vec3(0, 75, 0);
+		glm::vec3 velocity = Util::randomVec(-1, 1);
+		velocity.y = glm::max(0.5f, velocity.y) * 2;
+		velocity *= 5.0f;
+		return Particle{ initPos, velocity,
+			Util::randomPoint(0.5,1), //Gravity
+			Util::randomPoint(1, 8),  //Life length
+			Util::randomPoint(0,glm::pi<float>()), //rotationZ
+			Util::randomPoint(1,2) }; //scale
+	}
+	void Update (float dt)
+	{
+		for (auto & particle : particles)
+			if (!particle.Update (dt))
+				particle = GenParticle();
+	}
+	void Draw (RenderState & state)
+	{
+		prepareDraw();
+		for (auto const& particle : particles)
 		{
-			
-			this->texture = texture;
-			const int particleNum = 40;
-			particles.reserve (particleNum);
-			for (int i = 0 ; i < particleNum; i++)
-			{
-				particles.push_back(GenParticle ());
-			}
+			glm::mat4 MVP = QuadTexturer::CreateCameraFacingQuadMatrix (state, particle.position, glm::vec3(particle.scale), particle.rotationZ);
+			quadTexturer.Draw (texture, false,MVP);
 		}
-		Particle GenParticle ()
-		{
-			glm::vec3 initPos = glm::vec3(0, 75, 0);
-			glm::vec3 velocity = Util::randomVec(-1, 1);
-			velocity.y = glm::max(0.5f, velocity.y) * 2;
-			velocity *= 5.0f;
-			return Particle{ initPos, velocity,
-				Util::randomPoint(0.5,1), //Gravity
-				Util::randomPoint(1, 8),  //Life length
-				Util::randomPoint(0,glm::pi<float>()), //rotationZ
-				Util::randomPoint(1,2) }; //scale
+		endDraw();
+	}
+private:
+	void prepareDraw () 
+	{
+		if (type == RenderType::ADDITIVE) {
+			gl::Enable(gl::kDepthTest);
+			gl::DepthMask(false); //Need to disable depth writing, because then the particles behind it will be wrong
+			gl::Enable(gl::kBlend);
+			gl::BlendFunc(gl::kSrcAlpha, gl::kOne); //Additive blending	
 		}
-		void Update (float dt)
-		{
-			for (auto & particle : particles)
-				if (!particle.Update (dt))
-					particle = GenParticle();
+	}
+
+	void endDraw ()
+	{
+		if (type == RenderType::ADDITIVE) {
+			gl::DepthMask (true);
+			gl::Disable (gl::kBlend);
 		}
-		void Draw (RenderState & state)
-		{
-			//auto depth = gl::TemporaryDisable(gl::kDepthTest);
-			auto depth = gl::TemporaryEnable(gl::kDepthTest);
-			gl::DepthMask (false);
-			auto blend = gl::TemporaryEnable (gl::kBlend);
-			gl::BlendFunc(gl::kSrcAlpha, gl::kOne); //Additive blending
-			for (auto const& particle : particles)
-			{
-				glm::mat4 MVP = QuadTexturer::CreateCameraFacingQuadMatrix (state, particle.position, glm::vec3(particle.scale), particle.rotationZ);
-				quadTexturer.Draw (texture, false,MVP);
-			}
-			gl::DepthMask(true);
-		}
-	};
+	}
+};
 
 } // NS Particle
 } // NS Engine
